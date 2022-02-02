aa1fb2c3745f4f2c091a76afcf86ccff31e1fdfe
rosemadr <madi@redpill.me.uk>
2022-02-02 13:25:07 +0000

    completed controller web integration testing
    added deleteByIsbnTest()
    
    have 100% test coverage of BookController class

--- src/test/java/com/qa/controller/BookControllerWebIntegrationTests.java
+++ src/test/java/com/qa/controller/BookControllerWebIntegrationTests.java
@@ -32,6 +32,9 @@ public class BookControllerWebIntegrationTests {
 	// test data
 	private List<Book> books = new ArrayList<>();
 	private Book testBook;
+	private Book bookUpdateInfo;
+	private Book updatedTestBook;
+	private Long updatedBookIsbn;
 
 	@BeforeEach
 	public void init() {
@@ -39,6 +42,10 @@ public class BookControllerWebIntegrationTests {
 				new Book(9781492077992l, "Head First Design Patterns", "Freeman", "Eric", 2020, true, "O'Reilly", "UM"),
 				new Book(9780140237504l, "The Catcher in the Rye", "Salinger", "J.D.", 1946, false, "Penguin", "FB")));
 		testBook = new Book(9780141182902l, "The Trail", "Kafka", "Franz", 1925, false, "Penguin", "FB");
+		updatedTestBook = new Book(9780141182902l, "The Trail, English Translation", "Kafka", "Franz", 1994, false,
+				"Penguin", "FB");
+		bookUpdateInfo = new Book("The Trail, English Translation", "Kafka", "Franz", 1994, false, "Penguin", "FB");
+		updatedBookIsbn = 9780141182902l;
 	}
 
 	@Test
@@ -65,7 +72,6 @@ public class BookControllerWebIntegrationTests {
 		assertThat(expected).isEqualTo(actual);
 		// verify service called
 		verify(bookService, times(1)).getByIsbn(9780141182902l);
-
 	}
 
 	@Test
@@ -81,6 +87,32 @@ public class BookControllerWebIntegrationTests {
 		assertThat(expected).isEqualTo(actual);
 		// verify service called
 		verify(bookService, times(1)).createBook(testBook);
+	}
 
+	@Test
+	public void updateBookTest() {
+		// given
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "/books/" + updatedBookIsbn);
+		ResponseEntity<Book> expected = new ResponseEntity<Book>(updatedTestBook, headers, HttpStatus.ACCEPTED);
+		// when
+		when(bookService.updateBook(updatedBookIsbn, bookUpdateInfo)).thenReturn(updatedTestBook);
+		// then
+		ResponseEntity<Book> actual = controller.updateBook(updatedBookIsbn, bookUpdateInfo);
+		assertThat(expected).isEqualTo(actual);
+		// verify
+		verify(bookService, times(1)).updateBook(updatedBookIsbn, bookUpdateInfo);
+
+	}
+
+	@Test
+	public void deleteByIsbnTest() {
+		// given
+		ResponseEntity<?> expected = ResponseEntity.status(HttpStatus.ACCEPTED).build();
+		// then
+		ResponseEntity<?> actual = controller.deleteByIsbn(updatedBookIsbn);
+		assertThat(expected).isEqualTo(actual);
+		// verify
+		verify(bookService, times(1)).deleteByIsbn(updatedBookIsbn);
 	}
 }
